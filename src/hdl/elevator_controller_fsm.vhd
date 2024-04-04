bbd405e1e44b7fc91027eb879b2c1536dd8253c0
Sam <C26Sampson.McClung@afacademy.af.edu>
2024-04-03 18:52:43 -0600

    Fixed issue in which test cases would not properly run

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -82,7 +82,7 @@ end elevator_controller_fsm;
 
 -- Write the code in a similar style as the Lesson 19 ICE (stoplight FSM version 2)
 architecture Behavioral of elevator_controller_fsm is
-
+    
     -- Below you create a new variable type! You also define what values that 
     -- variable type can take on. Now you can assign a signal as 
     -- "sm_floor" the same way you'd assign a signal as std_logic
@@ -96,33 +96,40 @@ begin
 
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	-- Next State Logic
-    f_Q_next <= <state> when (<condition>) else -- going up
-                ...
-                ...
-                ... -- going down
-                ...
-                ... else
-                ...; -- default case
-  
+    f_Q_next <= s_floor2 when (f_Q=s_floor1 and i_up_down='1') else -- going up
+                s_floor3 when (f_Q=s_floor2 and i_up_down='1') else
+                s_floor4 when (f_Q=s_floor3 and i_up_down='1') else
+                s_floor3 when (f_Q=s_floor4 and i_up_down='0') else -- going down
+                s_floor2 when (f_Q=s_floor3 and i_up_down='0') else
+                s_floor1 when (f_Q=s_floor2 and i_up_down='0') else
+                f_Q; -- default case
+    
 	-- Output logic
     with f_Q select
-        o_floor <= <value> when s_floor1,
-                ...
-                ...
-                <value> when others; -- default is floor 2
+        o_floor <= "0001" when s_floor1,
+        "0010" when s_floor2,
+        "0011" when s_floor3,
+        "0100" when s_floor4,
+                "0010" when others; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
 	-- State memory ------------
 	register_proc : process (i_clk)
-    begin
-         -- synchronous reset
+            begin
+            --- state memory w/ asynchronous reset ---
+                if (rising_edge(i_clk)) then
+                    if i_reset = '1' then
+                        f_Q <= s_floor2;        -- reset state is floor 2
+                    elsif i_stop='0' then
+                        f_Q <= f_Q_next;
+                end if;    
+              end if;      
+            end process register_proc;
         
         -- if elevator is enabled, advance floors
         -- if not enabled, stay at current floor
-    
-	end process register_proc;	
 	
 	-------------------------------------------------------------------------------------------------------
 	
